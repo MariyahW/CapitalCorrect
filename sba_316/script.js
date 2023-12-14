37c541a8fb65af0313897c0afefe0753316ac61d
MariyahW <149527258+MariyahW@users.noreply.github.com>
2023-12-14 15:21:20 -0500

    Working on form validation, submenu works on home page but need a way to reload script for subsequent pages

--- sba_316/script.js
+++ sba_316/script.js
@@ -1,42 +1,69 @@
 const body = document.querySelector(`body`);
 let nav = document.querySelector(`nav-bar`);
-let dropdown=document.querySelector(`dropdown-menu`)
-const start=document.getElementById(`start`);
-const serv=document.getElementById(`serv`);
-const navButton = document.querySelector(`drop-down>button`);
+let dropdown = document.querySelector(`dropdown-menu`);
+const start = document.getElementById(`start`);
+const serv = document.getElementById(`serv`);
+const navButton = document.querySelector(`.button_`);
+const form = document.getElementById(`form-group`);
+const formName = document.getElementById(`name`);
+let error = document.querySelector(`err-mess`);
 
 const navLinks = [
-      { text: "Lending", href: `./lending` },
-      { text: `Credit Repair`, href: `/credit` },
-      { text: `Business Succession`, href: `./buyouts` },
-    ];
- 
+  { text: `All Services`, href: `./services.html` },
+  { text: "Lending", href: `https://www.davidallencapital.com/102159514` },
+  { text: `Credit Repair`, href: `/credit` },
+  { text: `Business Succession`, href: `./buyouts` },
+];
+
 navLinks.forEach((link) => {
-    let addOn = document.createElement(`a`);
-    addOn.setAttribute(`href`, link.href);
-    addOn.textContent = link.text;
-    dropdown.appendChild(addOn);
-  });
-  
+  let addOn = document.createElement(`a`);
+  addOn.setAttribute(`href`, link.href);
+  addOn.textContent = link.text;
+  dropdown.appendChild(addOn);
+});
 
-  
-  start.addEventListener(`click`,(event) =>{
-    event.stopPropagation();
-    location.href=`./contact.html`
+start.addEventListener(`click`, (event) => {
+  event.stopPropagation();
+  location.href = `./contact.html`;
+});
 
-  });
+serv.addEventListener(`click`, (event) => {
+  event.stopPropagation();
+  location.href = `./services.html`;
+});
 
-  serv.addEventListener(`click`,(event) =>{
-    event.stopPropagation();
-    location.href=`./services.html`
+// navButton.forEach(button=>{
+navButton.addEventListener("click", (event) => {
+  event.stopPropagation();
+  dropdown.classList.toggle("active");
+});
 
-  });
-  navButton.addEventListener('click',(event)=>{
-    event.stopPropagation();
-    if(navButton.className==`active`){
-      navButton.classList.remove(`active`);
-    }else{
-    navButton.classList.add(`active`);
-    }
+// });
 
-  })
+form.addEventListener(`submit`, (event) => {
+  debugger;
+
+  if (userVal()) {
+    event.preventDefault();
+    error.textContent = `Username must not contain special characters.`;
+    error.style.display = `block`;
+    setTimeout(() => {
+      error.style.display = `none`;
+    }, 2000);
+  }
+
+  if (email.value.includes(`example.com`)) {
+    event.preventDefault();
+    error.textContent = `example.com is not a valid domain name`;
+    error.style.display = `block`;
+    setTimeout(() => {
+      error.style.display = `none`;
+    }, 2000);
+  }
+});
+
+function userVal() {
+  const regEx = /[\W]/;
+  console.log(regEx.test(formName));
+  return regEx.test(formName);
+}
