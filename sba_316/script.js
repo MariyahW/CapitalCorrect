be47879106d00e842507fdf5ae0f9cba1723451d
MariyahW <149527258+MariyahW@users.noreply.github.com>
2023-12-14 00:04:38 -0500

    Working on site/project

--- sba_316/script.js
+++ sba_316/script.js
@@ -1,26 +1,19 @@
 const body = document.querySelector(`body`);
 let nav = document.querySelector(`nav-bar`);
+let dropdown=document.querySelector(`dropdown-menu`)
 const start=document.getElementById(`start`);
 const serv=document.getElementById(`serv`);
 const navLinks = [
-  { text: `home`, href: `./index.html` },
-  {
-    text: `Services`,
-    href: `#`,
-    subLinks: [
       { text: "Lending", href: `./lending` },
       { text: `Credit Repair`, href: `/credit` },
       { text: `Business Succession`, href: `./buyouts` },
-    ],
-  },
-  { text: `Contact Us`, href: `./contact.html` },
-];
-
+    ];
+ 
 navLinks.forEach((link) => {
     let addOn = document.createElement(`a`);
     addOn.setAttribute(`href`, link.href);
     addOn.textContent = link.text;
-    nav.appendChild(addOn);
+    dropdown.appendChild(addOn);
   });
   
   //Part 2-1
